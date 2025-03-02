f4698fa7c97164170fbbc1a89f18a2384fda690d
Jeremiah Augustin <Phantom4iife7@gmail.com>
2025-03-02 16:37:12 -0500

    Finished Project

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,47 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine("Guess my favorite number between 1 and 1000");
+
+            bool guessedCorrectly = false;
+
+            while (!guessedCorrectly)
+            {
+
+                Console.Write("Enter your guess: ");
+                int userInput;
+
+                //I'm still trying to get why Tryparse is even needed
+                while (!int.TryParse(Console.ReadLine(), out userInput))
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                    Console.Write("Enter your guess: ");
+                }
+
+                //if statements are objectively my favorite thing to do in C# so far
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("Too low, try again!");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("Too high, try again!");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it!!! My favorite number was {favNumber}.");
+                    guessedCorrectly = true;
+                }
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
