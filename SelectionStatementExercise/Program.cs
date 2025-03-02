0693d4138a2153af23a51aa0fd84859daa608304
Jeremiah Augustin <Phantom4iife7@gmail.com>
2025-03-02 17:46:58 -0500

    Updated it with the second exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,6 @@
     {
         static void Main()
         {
-            
             var r = new Random();
             var favNumber = r.Next(1, 1000);
 
@@ -14,18 +13,15 @@
 
             while (!guessedCorrectly)
             {
-
                 Console.Write("Enter your guess: ");
                 int userInput;
 
-                //I'm still trying to get why Tryparse is even needed
                 while (!int.TryParse(Console.ReadLine(), out userInput))
                 {
                     Console.WriteLine("Invalid input. Please enter a number.");
                     Console.Write("Enter your guess: ");
                 }
 
-                //if statements are objectively my favorite thing to do in C# so far
                 if (userInput < favNumber)
                 {
                     Console.WriteLine("Too low, try again!");
@@ -40,6 +36,35 @@
                     guessedCorrectly = true;
                 }
             }
+
+            Console.WriteLine("\nNow, let's talk about school subjects.");
+            Console.Write("What is your favorite school subject? ");
+            string favoriteSubject = Console.ReadLine().Trim().ToLower();
+
+            switch (favoriteSubject)
+            {
+                case "math":
+                    Console.WriteLine("Math is cool. That's also my favorite!");
+                    break;
+                case "science":
+                    Console.WriteLine("I always found science to be fascinating! It helps us understand the world around us.");
+                    break;
+                case "history":
+                    Console.WriteLine("History is extremely important! It teaches us how we got where we are, and why we live the way we do.");
+                    break;
+                case "english":
+                    Console.WriteLine("English is great for expressing thoughts and creativity.");
+                    break;
+                case "art":
+                    Console.WriteLine("Art allows creativity to flourish in amazing ways!");
+                    break;
+                default:
+                    Console.WriteLine("Interesting choice! What do you like about it?");
+                    string userResponse = Console.ReadLine();
+                    Console.WriteLine($"That's a plausible reason");
+                    break;
+            }
+
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
